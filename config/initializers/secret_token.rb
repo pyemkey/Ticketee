51e4433da7ac19e6da8d94cba83a09e9e4a66ef7
pyemkey <py.emkey@gmail.com>
2014-05-04 23:43:53 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ticketee::Application.config.secret_key_base = '9453b88f03d7018dbd21d8d1fb2fb6fe617dc93aa2b52f17aae850d4083827586d4241ba978551f5551c00025f9b627dbf3eae84c30cfc9a9f7b80cca57d0654'

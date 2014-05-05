23e866c603d186f346659955dc016fb60404abe8
pyemkey <py.emkey@gmail.com>
2014-05-05 17:36:09 +0200

    Redirect the users back to the projects page if they try going to a project that doesn't exist

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
   def index
     @projects = Project.all    
   end
@@ -18,15 +19,12 @@ class ProjectsController < ApplicationController
   end
 
   def show
-   @project = Project.find(params[:id]) 
   end
 
   def edit
-    @project = Project.find(params[:id])
   end
 
   def update
-    @project = Project.find(params[:id])
     if @project.update_attributes(project_params)
       redirect_to @project, notice: "Project has been updated"
     else
@@ -36,7 +34,6 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    @project = Project.find(params[:id])
     @project.destroy
 
     redirect_to root_path, notice: "Project has been destroyed."
@@ -45,5 +42,12 @@ class ProjectsController < ApplicationController
   private
   def project_params
     params.require(:project).permit(:name, :description)
+  end
+
+  def set_project
+    @project = Project.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:danger] = "The project you were looking for could not be found."
+    redirect_to projects_path
   end
 end

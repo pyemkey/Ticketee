230700e9bf9aab6bcc6128253be4167f63e0bbc3
pyemkey <py.emkey@gmail.com>
2014-05-05 21:57:20 +0200

    Implement displaying a list of relevant tickets.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140505164923) do
+ActiveRecord::Schema.define(version: 20140505184006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20140505164923) do
 
   create_table "tickets", force: true do |t|
     t.string   "title"
-    t.string   "description"
+    t.text     "description"
     t.integer  "project_id"
     t.datetime "created_at"
     t.datetime "updated_at"
